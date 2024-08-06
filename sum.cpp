b534c24b7993d738048993136cf2df7bbe8e9ada
Ridhimaaaa29 <158986245+Ridhimaaaa29@users.noreply.github.com>
2024-08-06 11:37:24 +0530

    Add files via upload
    Take user input and print the sum of two numbers.

---
+++ sum.cpp
@@ -0,0 +1,14 @@
+//Take user input and print the sum of two numbers
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    //user input
+    cout<<"Enter first number";
+    cin>>a;
+    cout<<"Enter second number";
+    cin>>b;
+    cout<<"Sum :"<<a+b;
+    return 0;
+}
