184c9b9b9c8417fe69f1ada6056d99dd134d304c
Ridhimaaaa29 <158986245+Ridhimaaaa29@users.noreply.github.com>
2024-08-06 11:26:15 +0530

    Add files via upload
    First OG Hello World! code

---
+++ Hello_World.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+  cout<<"Hello World!";
+  cout<<4 * 3 / 8 + 2.5 * 2<<endl;
+  cout<<(5 * 7.0 / 2 - 2.5) / 5 * 2<<endl;
+  cout<<41 % 7 * 3 / 5 + 5 / 2 * 2.5<<endl;
+
+//   // This is a comment. Above line will just print Hello World! On screen.
+
+//   /* This is an example of multi-line comment, 
+//   instead of one line you can also write multi-line comments which generally used to provide readability of code. 
+//   These must start end with proper constructs as shown here. */
+  
+//   // Return 0 to indicate normal termination
+  return 0;
+}
+
+
+
+
