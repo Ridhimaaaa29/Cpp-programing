98bb284173616439a7d7a558f6ff3168ff0ea7b8
Ridhimaaaa29 <158986245+Ridhimaaaa29@users.noreply.github.com>
2024-09-02 13:33:17 +0530

    Add files via upload

---
+++ sumofThreeNums.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+       
+    int a, b, c;
+    cin>> a >> b >> c; //Read the three integers
+    cout<< a + b + c; //Print the sum of three integers
+    return 0;
+}
