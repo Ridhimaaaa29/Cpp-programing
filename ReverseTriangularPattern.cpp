91230c5f8e1db0cac001b16088e70e940cfd405d
Ridhimaaaa29 <158986245+Ridhimaaaa29@users.noreply.github.com>
2024-09-02 13:40:17 +0530

    Add files via upload

---
+++ ReverseTriangularPattern.cpp
@@ -0,0 +1,19 @@
+//Reverse Triangular Pattern
+#include<iostream>
+using namespace std;
+int main() {
+    int n;
+    cout<<"enter n: ";
+    cin>>n;
+    for(int i = 1; i <= n ; i++){
+        for(int j = 1; j <= ((n+1)-i); j++){ //no. of asterics in coloumn = row no.
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+
+
+
